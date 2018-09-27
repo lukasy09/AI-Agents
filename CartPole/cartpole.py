d8d2d1605afd7e00dee236a4021351f78cc7e75f
Łukasz Bednarski <lukasy09@gmail.com>
2018-09-27 18:02:21 +0200

    Cartpole comments a little

--- CartPole/cartpole.py
+++ CartPole/cartpole.py
@@ -1,19 +1,21 @@
-import gym
-import random
-import numpy as np
-import tflearn
-from statistics import median, mean
+"""Imports. Working with OpenAI library"""
 
+import gym
+import numpy as np
+from statistics import mean
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils.np_utils import to_categorical
 
 
+#Creating environment
+def create_environment():
+    env = gym.make("CartPole-v1")
+    env.reset()
+    return env
 
-env = gym.make("CartPole-v1")
-env.reset()
-
-def initial_population(initial_games = 10000, goal_steps = 500, score_requirement = 120):
+#Returns the data that we put into network
+def initial_population(env,initial_games = 10000, goal_steps = 500, score_requirement = 120):
 
     training_data = []
     scores = []
@@ -27,7 +29,7 @@ def initial_population(initial_games = 10000, goal_steps = 500, score_requiremen
 
         for _ in range(goal_steps):
          
-            action = random.randrange(0,2)
+            action = env.action_space.sample()
 
             observation, reward, done, info = env.step(action)
        
@@ -35,7 +37,8 @@ def initial_population(initial_games = 10000, goal_steps = 500, score_requiremen
                 game_memory.append([prev_observation, action])
             prev_observation = observation
             score+=reward
-            if done: break
+            if done:
+                break
         
         if score >= score_requirement:
             accepted_scores.append(score)
@@ -46,68 +49,69 @@ def initial_population(initial_games = 10000, goal_steps = 500, score_requiremen
     
         scores.append(score)
     
-    training_data_save = np.array(training_data)
+    #training_data_save = np.array(training_data)
     #np.save('cartpole-v0.npy',training_data_save)
     
     print('Average accepted score:',mean(accepted_scores))
     
     return np.array(training_data), labels
 
+env = create_environment()
     
-
-train_data, labels = initial_population()
-
+train_data, labels = initial_population(env)
 labels = to_categorical(labels, 2)
 
-model = Sequential()
-model.add(Dense(units = 128, input_dim = 4))
-model.add(Dense(units = 256, activation = 'relu'))
-model.add(Dense(units = 512, activation = 'relu'))
-model.add(Dense(units = 256, activation = 'relu'))
-model.add(Dense(units = 128, activation = 'relu'))
-model.add(Dense(units = 2, activation = 'softmax'))
+# Skeleton of the model
+def create_model():
+    model = Sequential()
+    model.add(Dense(units = 128, input_dim = 4))
+    model.add(Dense(units = 256, activation = 'relu'))
+    model.add(Dense(units = 512, activation = 'relu'))
+    model.add(Dense(units = 256, activation = 'relu'))
+    model.add(Dense(units = 128, activation = 'relu'))
+    model.add(Dense(units = 2, activation = 'softmax'))
     
-model.compile(loss='categorical_crossentropy',
+    model.compile(loss='categorical_crossentropy',
               optimizer="adam",
               metrics=['accuracy'])
+    return model
 
+model = create_model()
 model.fit(train_data, labels, epochs = 10)
 
 #model.save("cartpole_model.h5")
 
 
-###
-scores = []
-choices = []
+def play_games(env,model, n_games = 10, steps = 500):
+    scores = []
+    choices = []
 
-for each_game in range(10):
-    score = 0
-    game_memory = []
-    prev_obs = []
-    env.reset()
-    
-    for t in range(10000):
-        env.render()
+    for each_game in range(n_games):
+        score = 0
+        game_memory = []
+        prev_obs = []
+        env.reset()
         
-        if len(prev_obs) == 0:
-            action = random.randrange(0, 2)
-        else:
-            action = np.argmax(model.predict(np.array([prev_obs])))
-        choices.append(action)
-        
-        new_obs , reward, done, info = env.step(action)
-        
-        prev_obs = new_obs
-        
-        game_memory.append([new_obs, action])
-        
-        score += reward
-        
-        if done:
-            break
+        for t in range(steps):
+            env.render()
+            
+            if len(prev_obs) == 0:
+                action = env.action_space.sample()
+            else:
+                    action = np.argmax(model.predict(np.array([prev_obs])))
+            choices.append(action)
+            
+            new_obs , reward, done, info = env.step(action)
+                    
+            prev_obs = new_obs
+                    
+            game_memory.append([new_obs, action])
+                    
+            score += reward
+                    
+            if done:
+                break
     scores.append(score)
-print("Avg:", mean(scores))
+    print("Avg:", mean(scores))
 
-
-
-
+play_games(env ,model)
